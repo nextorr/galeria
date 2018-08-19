b493ddb0ef4c719da66d59be3648528237b213ef
nextorr <nextorr@gmail.com>
2018-08-19 14:43:42 -0500

    remove ssl dbconnection
    removed the ssl argument on the database connection file|.

--- galeria/settings.py
+++ galeria/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'galeria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=500, require_ssl=True)}
+DATABASES = {'default': dj_database_url.config()}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
